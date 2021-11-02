49d049b661aa7e9f73102f855b9802ca6d5938f5
mktk24 <89533273+Markuski2023@users.noreply.github.com>
2021-11-02 13:06:00 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+
+def isLeapYear(year):
+    if year % 4 == 0 and year % 100 != 0:
+        print("Its a leap year")
+        return True
+    elif year % 400 == 0:
+        print("Its a leap year")
+        return True
+    elif year % 4 != 0:
+        print("Its NOT a leap year")
+        return False
+    elif year % 100 == 0 and year % 400 != 0:
+        print("Its NOT a leap year")
+        return False
+
+isLeapYear(50)
---
+++ tests.py
@@ -0,0 +1,14 @@
+from main import *
+
+def test_to_check_if_year_is_divisible_by_4_but_not_100():
+    assert True == isLeapYear(2024)
+
+def test_to_check_if_year_is_divisible_by_400():
+    assert True == isLeapYear(2000)
+
+def test_to_check_if_year_is_not_divisible_by_4():
+    assert True != isLeapYear(1705)
+
+def test_to_check_if_year_is_not_divisible_by_100_but_not400():
+    assert True != isLeapYear(1900)
+

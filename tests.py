9f8c29af3fe8f7107558016b338b15812ef3b8a3
mktk24 <89533273+Markuski2023@users.noreply.github.com>
2021-11-02 13:07:22 +0100

    Please work

--- tests.py
+++ tests.py
@@ -5,7 +5,6 @@ def test_to_check_if_year_is_divisible_by_4_but_not_100():
 
 def test_to_check_if_year_is_divisible_by_400():
     assert True == isLeapYear(2000)
-    assert True == isLeapYear(1600)
 
 def test_to_check_if_year_is_not_divisible_by_4():
     assert True != isLeapYear(1705)

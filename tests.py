caafe22ea7cdd4a714fd02415c3369cda732fcfe
mktk24 <89533273+Markuski2023@users.noreply.github.com>
2021-11-02 13:13:48 +0100

    Bad test

--- tests.py
+++ tests.py
@@ -5,6 +5,7 @@ def test_to_check_if_year_is_divisible_by_4_but_not_100():
 
 def test_to_check_if_year_is_divisible_by_400():
     assert True == isLeapYear(2000)
+    assert True == isLeapYear(1570)
 
 def test_to_check_if_year_is_not_divisible_by_4():
     assert True != isLeapYear(1705)
